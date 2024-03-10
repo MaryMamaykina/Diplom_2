ee62384178fe004e1a185264582aa9258343cbb7
MAMAYKINA\maria <MaryMamaykina@yandex.ru>
2024-03-10 18:03:35 +0300

    Create CreateUserTest

---
+++ src/test/java/CreateUserTest.java
@@ -0,0 +1,62 @@
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+import stellarburgers.dto.CreateUserSuccessfulResponse;
+import stellarburgers.dto.ErrorMessageResponse;
+import stellarburgers.staticmethodsandvariables.UserAPI;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class CreateUserTest {
+    UserAPI userAPI = new UserAPI();
+    @Test
+    public void doesCreatingNewUserWorks(){
+        String email = "sdhfhds@yandex.ru";
+        String password = "ffhhhj";
+        String name = "Ivan";
+        CreateUserSuccessfulResponse response = userAPI.createUser(email,password,name).as(CreateUserSuccessfulResponse.class);
+        Assert.assertTrue(response.isSuccess());
+
+
+        userAPI.deleteUser(response.getAccessToken().replace("Bearer ",""));
+    }
+    @Test
+    public void IsCreatingUserWhoAlreadyRegisteredImpossible(){
+        String email = "sdhfhds@yandex.ru";
+        String password = "ffhhhj";
+        String name = "Ivan";
+        CreateUserSuccessfulResponse response = userAPI.createUser(email,password,name).as(CreateUserSuccessfulResponse.class);
+        ErrorMessageResponse response2 = userAPI.createUser(email,password,name).as(ErrorMessageResponse.class);
+        Assert.assertFalse(response2.isSuccess());
+
+        userAPI.deleteUser(response.getAccessToken().replace("Bearer ",""));
+    }
+
+    @Test
+    public void IsCreatingUserWithoutEmailImpossible(){
+        String email = null;
+        String password = "ffhhhj";
+        String name = "Ivan";
+        ErrorMessageResponse response = userAPI.createUser(email,password,name).as(ErrorMessageResponse.class);
+        Assert.assertFalse(response.isSuccess());
+    }
+
+    @Test
+    public void IsCreatingUserWithoutPasswordImpossible(){
+        String email = "sdhfhds@yandex.ru";
+        String password = null;
+        String name = "Ivan";
+        ErrorMessageResponse response = userAPI.createUser(email,password,name).as(ErrorMessageResponse.class);
+        Assert.assertFalse(response.isSuccess());
+    }
+
+    @Test
+    public void IsCreatingUserWithoutNameImpossible(){
+        String email = "sdhfhds@yandex.ru";
+        String password = "ffhhhj";
+        String name = null;
+        ErrorMessageResponse response = userAPI.createUser(email,password,name).as(ErrorMessageResponse.class);
+        Assert.assertFalse(response.isSuccess());
+    }
+
+}
