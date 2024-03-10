5ae79c3c5142c7f96821690fd4513573af3d3c2a
MAMAYKINA\maria <MaryMamaykina@yandex.ru>
2024-03-10 17:59:16 +0300

    Create dto files for CreateTest

---
+++ src/main/java/stellarburgers/dto/ErrorMessageResponse.java
@@ -0,0 +1,15 @@
+package stellarburgers.dto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessageResponse {
+    private boolean success;
+    private String message;
+}
