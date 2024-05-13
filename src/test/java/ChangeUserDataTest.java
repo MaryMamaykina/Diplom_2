d7fe59dfb5c88bfe559e93cef9f2dc112cbaafc4
MAMAYKINA\maria <MaryMamaykina@yandex.ru>
2024-05-13 08:29:29 +0300

    Added some tests in ChangeUserDataTest

--- src/test/java/ChangeUserDataTest.java
+++ src/test/java/ChangeUserDataTest.java
@@ -26,6 +26,36 @@ public class ChangeUserDataTest {
         userAPI.deleteUser(accessToken);
     }
     @Test
+    public void doesChangingUserDataWithAuthorizationAllowsChangeName() {
+        String email = "sdhfhud@yandex.ru";
+        String password = "ffhhhj";
+        String name = "Ivan";
+        String newName = "cuteIvan";
+        CreateUserSuccessfulResponse response1 = userAPI.createUser(email,password,name).as(CreateUserSuccessfulResponse.class);
+        String accessToken = response1.getAccessToken().replace("Bearer ","");
+        userAPI.changeUserData(email, newName, accessToken).then().statusCode(SC_OK)
+                .and()
+                .body("success", equalTo(true))
+                .and()
+                .body("user.name", equalTo(newName));
+        userAPI.deleteUser(accessToken);
+    }
+    @Test
+    public void doesChangingUserDataWithAuthorizationAllowsChangeEmail() {
+        String email = "sdhfhud@yandex.ru";
+        String password = "ffhhhj";
+        String name = "Ivan";
+        String newEmail = "like@yandex.ru";
+        CreateUserSuccessfulResponse response1 = userAPI.createUser(email,password,name).as(CreateUserSuccessfulResponse.class);
+        String accessToken = response1.getAccessToken().replace("Bearer ","");
+        userAPI.changeUserData(name, newEmail, accessToken).then().statusCode(SC_OK)
+                .and()
+                .body("success", equalTo(true))
+                .and()
+                .body("user.name", equalTo(newEmail));
+        userAPI.deleteUser(accessToken);
+    }
+    @Test
     public void doesNotChangingUserDataWithoutAuthorizationWork() {
         String newEmail = "like@yandex.ru";
         String newName = "cuteIvan";
